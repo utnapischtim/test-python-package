72c698c48a33b3a78e6928f6b33b014ae1f9f532
Christoph Ladurner <christoph.ladurner@tugraz.at>
2022-09-15 13:39:06 +0200

    fix

--- test_python_package/ext.py
+++ test_python_package/ext.py
@@ -5,7 +5,7 @@
 # test-python-package is free software; you can redistribute it and/or modify
 # it under the terms of the MIT License; see LICENSE file for more details.
 
-""""test python package""""
+"""test python package"""
 
 from flask_babelex import gettext as _
 
@@ -20,23 +20,23 @@ class TestPythonPackage(object):
         # TODO: This is an example of translation string with comment. Please
         # remove it.
         # NOTE: This is a note to a translator.
-        _('A translation string')
+        _("A translation string")
         if app:
             self.init_app(app)
 
     def init_app(self, app):
         """Flask application initialization."""
         self.init_config(app)
-        app.extensions['test-python-package'] = self
+        app.extensions["test-python-package"] = self
 
     def init_config(self, app):
         """Initialize configuration."""
         # Use theme's base template if theme is installed
-        if 'BASE_TEMPLATE' in app.config:
+        if "BASE_TEMPLATE" in app.config:
             app.config.setdefault(
-                'TEST_PYTHON_PACKAGE_BASE_TEMPLATE',
-                app.config['BASE_TEMPLATE'],
+                "TEST_PYTHON_PACKAGE_BASE_TEMPLATE",
+                app.config["BASE_TEMPLATE"],
             )
         for k in dir(config):
-            if k.startswith('TEST_PYTHON_PACKAGE_'):
+            if k.startswith("TEST_PYTHON_PACKAGE_"):
                 app.config.setdefault(k, getattr(config, k))
--- test_python_package/views.py
+++ test_python_package/views.py
@@ -5,7 +5,7 @@
 # test-python-package is free software; you can redistribute it and/or modify
 # it under the terms of the MIT License; see LICENSE file for more details.
 
-""""test python package""""
+"""test python package"""
 
 # TODO: This is an example file. Remove it if you do not need it, including
 # the templates and static folders as well as the test case.
@@ -14,10 +14,10 @@ from flask import Blueprint, render_template
 from flask_babelex import gettext as _
 
 blueprint = Blueprint(
-    'test_python_package',
+    "test_python_package",
     __name__,
-    template_folder='templates',
-    static_folder='static',
+    template_folder="templates",
+    static_folder="static",
 )
 
 
@@ -25,5 +25,5 @@ blueprint = Blueprint(
 def index():
     """Render a basic view."""
     return render_template(
-        "test_python_package/index.html",
-        module_name=_('test-python-package'))
+        "test_python_package/index.html", module_name=_("test-python-package")
+    )
